28005b05df367c4d1410da0a96bc82e95197ca72
Dotpunkto <Dotpunkt0@vivaldi.net>
2021-04-17 00:56:41 +0200

    test

--- CommandeTest/PizzaControllerTest.cs
+++ CommandeTest/PizzaControllerTest.cs
@@ -1,16 +1,5 @@
-using System.Net;
-using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Xunit;
-using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
-using commandezzz.Controllers;
 using commandezzz.IServices;
-using System.Web.Http;
-using commandezzz.Model;
-using System.Collections.Generic;
-using MyTested.WebApi;
 
 namespace CommandeTest
 {
